7cc1b03b30578092545365732e0d79a72324da55
Vignesh K G <59288428+Vignesh-24@users.noreply.github.com>
2021-03-09 15:11:43 +0530

    Delete excelPDF directory

--- excelPDF/TextToPDF.java
+++
@@ -1,5 +0,0 @@
-package excelPDF;
-
-public interface TextToPDF {
-	public void writeToPDF(String xmlfile) throws Exception;
-}
--- excelPDF/excelPDF/TextToExcel.java
+++
@@ -1,7 +0,0 @@
-package excelPDF;
-
-interface TextToExcel {
-	public void writeToExcel(String xmlfile) throws Exception;
-
-}
-
