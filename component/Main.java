c54aec2554411209127a45dce9a9af84a76e3dca
Vignesh K G <59288428+Vignesh-24@users.noreply.github.com>
2021-03-15 14:57:40 +0530

    Updated Main

--- component/Main.java
+++ component/Main.java
@@ -9,6 +9,8 @@ import component.mail.MailSender;
 import component.mail.XMLtoEmail;
 import component.pdf.PDFWriter;
 import component.pdf.XMLtoPDF;
+import component.sms.MessageSender;
+import component.sms.XMLtoSMS;
 
 
 public class Main {
@@ -16,12 +18,12 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		
 		Object obj=new Main();
-		obj=Proxy.newProxyInstance(Main.class.getClassLoader(), new Class[] {XMLtoExcel.class,XMLtoPDF.class, XMLtoEmail.class}, 
+		obj=Proxy.newProxyInstance(Main.class.getClassLoader(), new Class[] {XMLtoExcel.class,XMLtoPDF.class, XMLtoEmail.class,XMLtoSMS.class}, 
 				new NewInvocationHandler(
-				new Object[]{new ExcelWriter(),new PDFWriter(), new MailSender()}));
+				new Object[]{new ExcelWriter(),new PDFWriter(), new MailSender(),new MessageSender()}));
 		
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Select Choice\n1.Excel\n2.PDF\n3.Email\n");
+		System.out.println("Select Choice\n1.Excel\n2.PDF\n3.Email\n4.SMS\n");
 		int n=sc.nextInt();
 		
 		
@@ -38,6 +40,10 @@ public class Main {
 			XMLtoEmail mail=(XMLtoEmail)obj;
 			System.out.println("Enter recipient mail address:\n");
 			mail.sendEmail(sc.next(),"src/component/bill.xml");
+		case 4:
+			XMLtoSMS sms=(XMLtoSMS)obj;
+			System.out.println("Enter recipient Phone Number:\n");
+			sms.sendSMS(sc.next(),"src/component/bill.xml");
 		default:
 			break;
 		}
